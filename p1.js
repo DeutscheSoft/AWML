e91e1526972690b6c68252a646a4f9c332738c23
Tobias S. Josefowitz <tobij@tobij.de>
2015-12-28 23:15:53 +0100

    more generic prototype should be able to give access to all the widgets.

--- p1.js
+++ p1.js
@@ -1,38 +1,72 @@
 "use strict";
+var P1 = {options: {}};
 (function(w) {
+  function parse(name, x) {
+      var match;
+
+      if (match = x.match(/^js:(.*)/m)) {
+          x = eval(match[1]);
+      } else if (match = x.match(/^inherit:(.*)/m)) {
+          x = P1.options[match[1]];
+      } else if (Number.parseFloat(x).toString() == x) {
+          x = Number.parseFloat(x);
+      }
+
+      return x;
+
+  }
   w.P1 = {
     registerWidget: function registerWidget(tagName, widget) {
       var proto = Object.create(HTMLElement.prototype);
+      proto.rec_find_children = function rec_find_children(node) {
+        var i;
+        var ret = [];
+
+        for (i = 0; i < node.childNodes.length; i++) {
+            if (node.childNodes[i].is_toolkit_node) {
+                ret.push(node.childNotes[i].widget);
+            } else if (node.childNodes[i]
+                       != this.widget.element) {
+                ret = ret.concat(this.rec_find_children(node.childNodes[i]));
+            }
+        }
+        return ret;
+      };
       proto.createdCallback = function() {
         var options = {
           container: this,
         };
+        var merge_options;
         var attr = this.attributes;
         var O = widget.prototype._options;
         console.log(O);
         for (var i = 0; i < attr.length; i++) {
-          var name = attr[i].name;
-          var value = attr[i].value;
-          switch (O[name]) {
-          case "number":
-            value = parseFloat(value); 
-            break;
-          case "int":
-            value = parseInt(value); 
-            break;
-          case "string":
-            break;
-          case "function":
-            value = new Function("", value);
-            break;
-          default:
-            console.log("Unsupported type %o for %o=%o\n", O[name], name, value);
-            continue;
-          }
-          options[name] = value;
+            var name = attr[i].name;
+            var value = attr[i].value;
+
+            if (name == "options") {
+                merge_options = P1.options[value];
+            }
+            options[name] = parse(name, value);
         }
+        if (merge_options) {
+            for (var x in options) {
+                merge_options[x] = options[x];
+            }
+            options = merge_options;
+        }
+        options.element = this;
         this.widget = new widget(options);
+        this.widget.add_children(this.rec_find_children(this));
         this.widget.show();
+      };
+      proto.appendChild = function(node) {
+        Node.prototype.appendChild.call(this, node);
+        this.widget.add_children(this.rec_find_children(node));
+      };
+      proto.insertBefore = function(n, p) {
+        Node.prototype.insertBefore.call(this, n, p);
+        this.widget.add_children(this.rec_find_children(n));
       };
       proto.is_toolkit_node = true;
       return document.registerElement(tagName, { prototype: proto, });
