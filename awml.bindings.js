a15984666fc789fe75687e069f26b667047142e8
Arne Goedeke <el@laramies.com>
2016-06-17 17:12:11 +0200

    AWML.Connectors: refactoring
    - Clear semantical seperation of a binding and connectors.
    - Implemented a Base Connector class which handles transformations.

--- awml.bindings.js
+++ awml.bindings.js
@@ -24,44 +24,46 @@
     this.has_value = false;
     this.listeners = [];
   };
-  AWML.Binding = Binding;
-  Binding.prototype = {};
-  Binding.prototype.set_handler = function(handler) {
-    this.handler = handler;
-    handler.register(this);
-    if (this.requested_value !== this.value) {
-      handler.set(this.uri, this.requested_value);
-    }
-  };
-  Binding.prototype.remove_handler = function(handler) {
-    handler.unregister(this);
-    this.handler = null;
-  };
-  Binding.prototype.addListener = function(callback) {
-    this.listeners.push(callback);
-    if (this.has_value) callback(this.value);
-  };
-  Binding.prototype.removeListener = function(callback) {
-    var i;
-    while ((i = this.listeners.indexOf(callback)) != -1)
-      this.listeners.splice(i, 1);
-  };
-  Binding.prototype.set = function(value) {
-    this.requested_value = value;
-    if (this.handler) {
+  Binding.prototype = {
+    set_handler: function(handler) {
+      this.handler = handler;
+      handler.register(this);
       if (this.requested_value !== this.value) {
-        this.handler.set(this.uri, value);
+        handler.set(this.uri, this.requested_value);
       }
-    }
+    },
+    remove_handler: function(handler) {
+      handler.unregister(this);
+      this.handler = null;
+    },
+    addListener: function(callback) {
+      this.listeners.push(callback);
+      if (this.has_value) callback(this.value);
+    },
+    removeListener: function(callback) {
+      var i;
+      while ((i = this.listeners.indexOf(callback)) != -1)
+        this.listeners.splice(i, 1);
+    },
+    set: function(value) {
+      this.requested_value = value;
+      if (this.handler) {
+        if (this.requested_value !== this.value) {
+          this.handler.set(this.uri, value);
+        }
+      }
+    },
+    update: function(value) {
+      var i;
+      this.value = value;
+      this.has_value = true;
+      for (i = 0; i < this.listeners.length; i++) {
+        this.listeners[i].call(this, value);
+      }
+    },
   };
-  Binding.prototype.update = function(value) {
-    var i;
-    this.value = value;
-    this.has_value = true;
-    for (i = 0; i < this.listeners.length; i++) {
-      this.listeners[i].call(this, value);
-    }
-  };
+  AWML.Binding = Binding;
+
   AWML.get_binding = function(uri) {
     var i = uri.search(':');
     var bind;
@@ -75,120 +77,154 @@
     if (handlers[proto]) bind.set_handler(handlers[proto]);
     return bind;
   };
-  var UserBinding = function(uri, widget, option, transform, transform_back) {
-    this.uri = uri;
+
+  /**
+   * Abstract Connector base class.
+   */
+  function Connector(binding, transform_in, transform_out) {
+    this.binding = binding;
+    this.uri = binding.uri;
+    this.transform_in = transform_in;
+    this.transform_out = transform_out;
+    this._receive = this.receive.bind(this, transform_in)
+    this._send = this.send.bind(this, transform_out);
+  };
+  Connector.prototype = {
+    receive: function(transform, v) { },
+    send: function(transform, v) {
+      var binding = this.binding;
+      if (transform) v = transform(v);
+      binding.set(v);
+    },
+    deactivate: function() {
+      this.binding.removeListener(this._receive);
+      return this;
+    },
+    activate: function() {
+      this.binding.addListener(this._receive);
+      return this;
+    },
+  };
+
+  /** 
+   * UserBinding is a Connector, which connects a toolkit widget and a binding.
+   * It binds to the <code>useraction</code> event of the widget and does not react to
+   * external <code>set</code> events.
+   *
+   */
+  function UserBinding(uri, widget, option, transform_in, transform_out) {
     this.widget = widget;
     this.option = option;
-    this.transform = transform;
-    this.transform_back = transform_back;
-    this.binding = AWML.get_binding(uri);
-    this._useraction_cb = function(key, value) {
-      if (key === option) {
-        if (transform_back) value = transform_back(value);
-        this.binding.set(value); 
-      }
-    }.bind(this);
-    this._set_cb = function(value) {
-      if (transform) value = transform(value);
-      this.widget.set(option, value);
-    }.bind(this);
+
+    this._useraction_cb = function(option, key, value) {
+      if (key === option) this._send(value);
+    }.bind(this, option);
+
+    Connector.call(this, AWML.get_binding(uri), transform_in, transform_out);
   };
-  UserBinding.prototype = {};
-  UserBinding.prototype.activate = function() {
-      this.binding.addListener(this._set_cb);
+  UserBinding.prototype = Object.assign(Object.create(Connector.prototype), {
+    activate: function() {
       this.widget.add_event("useraction", this._useraction_cb);
-      return this;
-  };
-  UserBinding.prototype.deactivate = function() {
-      this.binding.removeListener(this._set_cb);
+      return Connector.prototype.activate.call(this);
+    },
+    deactivate: function() {
       this.widget.remove_event("useraction", this._useraction_cb);
-      return this;
-  };
+      return Connector.prototype.deactivate.call(this);
+    },
+    receive: function(transform, v) {
+      if (transform) v = transform(v);
+      this.widget.set(this.option, v);
+    }
+  });
   AWML.UserBinding = UserBinding;
-  var SyncBinding = function(uri, widget, option, transform, transform_back) {
-    this.uri = uri;
+
+  /**
+   * SyncBinding
+   */
+  function SyncBinding(uri, widget, option, transform_in, transform_out) {
     this.widget = widget;
     this.option = option;
     this.recurse = false;
-    this.transform = transform;
-    this.transform_back = transform_back;
-    this.binding = AWML.get_binding(uri);
-    this._widget_cb = function(key, value) {
-      if (key === option && !this.recurse) {
-        this.recurse = true;
-        if (transform_back) value = transform_back(value);
-        this.binding.set(value); 
-        this.recurse = false;
-      }
-    }.bind(this);
-    this._set_cb = function(value) {
-      if (!this.recurse) {
-        this.recurse = true;
-        if (transform) value = transform(value);
-        this.widget.set(option, value);
-        this.recurse = false;
-      }
-    }.bind(this);
+
+    Connector.call(this, AWML.get_binding(uri), transform_in, transform_out);
   };
-  SyncBinding.prototype = {};
-  SyncBinding.prototype.activate = function() {
-      this.binding.addListener(this._set_cb);
-      this.widget.add_event("set", this._widget_cb);
-      return this;
-  };
-  SyncBinding.prototype.deactivate = function() {
-      this.binding.removeListener(this._set_cb);
-      this.widget.remove_event("set", this._widget_cb);
-      return this;
-  };
+  SyncBinding.prototype = Object.assign(Object.create(Connector.prototype), {
+    activate: function() {
+      this.widget.add_event("set_"+this.option, this._send);
+      return Connector.prototype.activate.call(this);
+    },
+    deactivate: function() {
+      this.widget.remove_event("set_"+this.option, this._send);
+      return Connector.prototype.deactivate.call(this);
+    },
+    receive: function(transform, v) {
+      if (this.recurse) return;
+      if (transform) v = transform(v);
+      this.recurse = true;
+      this.widget.set(this.option, v);
+      this.recurse = false;
+    },
+    send: function(transform, v) {
+      if (this.recurse) return;
+      this.recurse = true;
+      Connector.prototype.send.call(this, transform, v);
+      this.recurse = false;
+    },
+  });
   AWML.SyncBinding = SyncBinding;
-  var PropertyBinding = function(uri, target, property, transform, transform_back) {
-    this.uri = uri;
-    this.binding = AWML.get_binding(uri);
+
+  /**
+   * Property Binding, essentially only receives values.
+   */
+  function PropertyBinding(uri, target, property, transform_in, transform_out) {
     this.target = target;
     this.property = property;
-    this.transform = transform;
-    this.transform_back = transform_back;
-    this._set_cb = function(value) {
-      if (transform) value = transform(value);
-      this.target[this.property] = value;
-    }.bind(this);
+
+    Connector.call(this, AWML.get_binding(uri), transform_in, transform_out);
   };
-  PropertyBinding.prototype = {};
-  PropertyBinding.prototype.activate = function() {
-      var value;
-      this.binding.addListener(this._set_cb);
-      if (!this.binding.has_value) {
-        value = this.target[this.property];
-        if (this.transform_back)
-            value = this.transform_back(value);
-        this.binding.set(value);
-      }
-      return this;
-  };
-  PropertyBinding.prototype.deactivate = function() {
-      this.binding.removeListener(this._set_cb);
-      return this;
-  };
+  PropertyBinding.prototype = Object.assign(Object.create(Connector.prototype), {
+    activate: function() {
+      // TODO: might be more reasonable to require calling this explicitly
+      if (!this.binding.has_value)
+        this.sync();
+
+      return Connector.prototype.activate.call(this);
+    },
+    sync: function() {
+      this._send(this.target[this.property]);
+    },
+    receive: function(transform, v) {
+      if (transform) v = transform(v);
+      this.target[this.property] = v;
+    },
+  });
   AWML.PropertyBinding = PropertyBinding;
-  var MethodBinding = function(uri, method) {
+
+  /**
+   * MethodBinding calls a setter on receiving a value.
+   */
+  function MethodBinding(uri, method, transform_in, transform_out) {
     this.uri = uri;
     this.binding = AWML.get_binding(uri);
     this.method = method;
-    this._set_cb = function(value) {
-      this.method(value);
-    }.bind(this);
+    Connector.call(this, AWML.get_binding(uri), transform_in, transform_out);
   };
-  MethodBinding.prototype = {};
-  MethodBinding.prototype.activate = function() {
-      this.binding.addListener(this._set_cb);
-      return this;
-  };
-  MethodBinding.prototype.deactivate = function() {
-      this.binding.removeListener(this._set_cb);
-      return this;
-  };
+  MethodBinding.prototype = Object.assign(Object.create(Connector.prototype), {
+    receive: function(transform, v) {
+      if (transform) v = transform(v);
+      this.method(v);
+    },
+  });
   AWML.MethodBinding = MethodBinding;
+
+  AWML.Connectors = {
+    Base: Connector,
+    Method: MethodBinding,
+    Property: PropertyBinding,
+    TKUser: UserBinding,
+    TKSync: SyncBinding,
+  };
+
   AWML.Tags.Binding = document.registerElement("awml-binding", {
     prototype: Object.assign(Object.create(HTMLElement.prototype), {
       createdCallback: function() {
