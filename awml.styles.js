a35942ec9ded0a837cace7e61547ee938157c6da
Arne Goedeke <el@laramies.com>
2017-02-21 17:50:12 +0100

    added AWML-ATTRIBUTES

--- awml.styles.js
+++ awml.styles.js
@@ -91,4 +91,52 @@
       AWML.PrefixLogic.bind.call(this, src);
     },
   }));
+
+  function remove_attributes(node, attr) {
+    if (!attr) return;
+    for (var name in attr) {
+      node.removeAttribute(name);
+    }
+  }
+
+  function add_attributes(node, attr) {
+    if (!attr) return;
+    for (var name in attr) {
+      node.setAttribute(name, attr[name]);
+    }
+  }
+
+  AWML.Tags.Attributes = AWML.register_element("awml-attributes", Object.assign({}, AWML.PrefixLogic, AWML.RedrawLogic, {
+    createdCallback: function() {
+      var O = {};
+      this.awml_data = O;
+      AWML.PrefixLogic.createdCallback.call(this);
+      AWML.RedrawLogic.createdCallback.call(this);
+      O.get_attributes = null;
+      O.prev_attributes = null;
+      this.style.display = 'none';
+    },
+    receive: function(v) {
+      this.trigger_redraw();
+    },
+    redraw: function() {
+      var O = this.awml_data;
+      var v = O.binding.value;
+      if (!O.get_attributes)
+        O.get_attributes = AWML.parse_format("js", this.textContent, id);
+      var transform = O.transform_receive;
+      if (transform) v = transform(v);
+      var s = O.get_attributes(v);
+      remove_attributes(this.parentNode, O.prev_attributes);
+      O.prev_attributes = Object.keys(s);
+      add_attributes(this.parentNode, s);
+    },
+    unbind: function(src) {
+      var O = this.awml_data;
+      this.remove_redraw();
+      remove_attributes(this.parentNode, O.prev_attributes);
+      AWML.PrefixLogic.bind.call(this, src);
+    },
+  }));
+
 })(this.AWML || (this.AWML = {}));
