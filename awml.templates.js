6006e89ad791efd3c0cff3d7432b61ea27fcbf99
Arne Goedeke <el@laramies.com>
2018-08-27 09:15:00 +0200

    AWML-CLONE: use XHR as fallback
    Some older browsers do not support window.fetch.

--- awml.templates.js
+++ awml.templates.js
@@ -20,17 +20,41 @@
     return new URL(path, base).href;
   }
 
+  function fetch_text(url) {
+    if ('fetch' in window) {
+      return fetch(url).then(function(response) {
+        if (!response.ok) throw new Error(response.statusText);
+        return response.text();
+      });
+    } else {
+      return new Promise(function(resolve, reject) {
+        var r = new XMLHttpRequest();
+        r.addEventListener("readystatechange", function() {
+          if (r.readyState === 4) {
+            if (r.status === 200) {
+              resolve(r.responseText);
+            } else {
+              reject("Error: " + r.status);
+            }
+          }
+        });
+        r.addEventListener("error", function(ev) {
+          reject(ev);
+        });
+        r.open("GET", url);
+        r.send();
+      });
+    }
+  }
+
   function fetch_template(path, base) {
     var url = get_url(path, base);
 
-    return AWML.register_loading(fetch(url).then(function(response) {
-      if (!response.ok) throw new Error(response.statusText);
-      return response.text().then(function(text) {
-        var template = document.createElement("TEMPLATE");
-        template.url = url;
-        template.innerHTML = text;
-        return template;
-      });
+    return AWML.register_loading(fetch_text(url).then(function(text) {
+      var template = document.createElement("TEMPLATE");
+      template.url = url;
+      template.innerHTML = text;
+      return template;
     }));
   }
 
