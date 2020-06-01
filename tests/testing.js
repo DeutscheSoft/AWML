e803a7dae2f1a411b67051302f9ade4dab13eb21
Arne Goedeke <arne@deuso.de>
2020-06-01 17:30:54 +0200

    [testsuite]: added test runner

--- tests/testing.js
+++ tests/testing.js
@@ -1,3 +1,41 @@
 export function assertEqual(a, b) {
   if (a !== b) throw new Error('Assertion failed.');
 }
+
+export function done() {
+  window.parent.postMessage({ ok: true });
+}
+
+export function failure(err) {
+  window.parent.postMessage({ ok: false, error: err.toString() });
+}
+
+export function define(callback) {
+  try {
+    let called = false;
+    const p = callback(() => {
+      called = true;
+    });
+
+    if (typeof p === 'object' && typeof p.then === 'function') {
+      p.then(
+        () => {
+          done();
+        },
+        (error) => {
+          failure(error);
+        }
+      );
+    } else if (!called) {
+      done();
+    }
+  } catch (error) {
+    failure(error);
+  }
+}
+
+export function waitForFrame() {
+  return new Promise((resolve) => {
+    requestAnimationFrame(resolve);
+  });
+}
