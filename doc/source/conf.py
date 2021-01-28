d13186a36fb079602c76af4cfbfd3fa8b66c2d58
Matt Hardy <matt@deuso.de>
2021-01-28 16:07:47 +0100

    Add default file ref to 'index'

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,6 +27,7 @@ author = 'Arne Goedeke'
 release = '2.0'
 
 # -- General configuration ---------------------------------------------------
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
