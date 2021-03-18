1cc7b4f60cda342cf3caa4c7c2fa513b9873d77e
Arne Goedeke <arne@deuso.de>
2021-03-18 10:37:26 +0100

    [testsuite]: Add missing test helper

---
+++ tests/lib/handle_websocket.js
@@ -0,0 +1,14 @@
+const handled = new WeakSet();
+
+export function handleWebSocket(request, cb) {
+  if (handled.has(request)) return;
+
+  handled.add(request);
+
+  cb();
+}
+
+export function closeUnhandledWebSockets(request, socket, head) {
+  if (!handled.has(request))
+    socket.end();
+}
