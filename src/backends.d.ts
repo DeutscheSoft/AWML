6f685d96a7a00c3def2c71c18e0f2a2edfc56f82
Arne Goedeke <arne@deuso.de>
2022-03-01 20:34:49 +0100

    Add typescript headers for more backend APIs

--- src/backends.d.ts
+++ src/backends.d.ts
@@ -1,3 +1,11 @@
 import { BackendValue } from './backend_value';
+import { BackendBase } from './backends/backend_base';
 
 export function getBackendValue<T>(address: string): BackendValue<T>;
+
+export function getBackend(name: string): BackendBase;
+export function waitForBackend(name: string): Promise<BackendBase>;
+export function registerBackend(name: string, backend: BackendBase): void;
+export function unregisterBackend(name: string, backend: BackendBase): void;
+export function getBackendValues(backendName: string): Map<string,BackendValue<any>>;
+export function getBackends(): Map<string,BackendBase>;
