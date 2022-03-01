fe8c7e5aa047044913abaf338a9dadf072f32cdc
Arne Goedeke <arne@deuso.de>
2022-03-01 17:27:28 +0100

    Add typescript test for getBackendValue

---
+++ ts_tests/backend_value.success.ts
@@ -0,0 +1,19 @@
+import { getBackendValue } from '../src/index';
+
+{
+  getBackendValue('foo:bar').set(234);
+  getBackendValue<number>('foo:bar').set(234);
+  getBackendValue<number>('foo:bar').subscribe((value: number) => {
+    console.log('value:', value*2);
+  });
+  getBackendValue<string>('foo:bar').subscribe((value: string) => {
+    console.log('value:', value);
+  });
+}
+
+{
+  const run = async () => {
+    const n: number = await getBackendValue<number>('foo:bar').wait();
+    const str: string = await getBackendValue<string>('foo:bar').wait();
+  };
+}
