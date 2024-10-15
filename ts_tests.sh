c5c9e1f5c4b987220cf8cbe6c8d8c88710a2c5a9
Arne Goedeke <arne@deuso.de>
2024-10-15 20:36:14 +0200

    [testsuite]: Use es2018 for typescript tests

--- ts_tests.sh
+++ ts_tests.sh
@@ -1,9 +1,9 @@
 echo -n Running tests ts_tests/*.success.ts
-./node_modules/.bin/tsc --lib es2015,dom --noEmit ts_tests/*.success.ts || exit 1
+npx --package typescript -- tsc --lib es2018,dom --noEmit ts_tests/*.success.ts || exit 1
 echo " OK"
 for file in ts_tests/*.fail.ts; do
   echo -n Running test $file ...
-  ./node_modules/.bin/tsc --lib es2015,dom --noEmit $file &> /dev/null \
+  npx --package typescript -- tsc --lib es2018,dom --noEmit $file &> /dev/null \
     && echo "Unexpected pass." && exit 1
   echo " OK"
 done
