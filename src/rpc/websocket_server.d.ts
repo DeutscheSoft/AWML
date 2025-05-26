0ceed1e637499d08700874f748a82f97dab2167b
Arne Goedeke <arne@deuso.de>
2025-05-26 19:37:56 +0200

    WebSocketRPCServer: Make event handler accessible
    In order to support different websocket module
    APIs we expose handlers for message, error
    and close.

--- src/rpc/websocket_server.d.ts
+++ src/rpc/websocket_server.d.ts
@@ -4,4 +4,8 @@ export class WebSocketRPCServer extends RPCServerBase {
   isClosed(): boolean;
   close(): void;
   constructor(websocket: unknown, methods: RPCMethods);
+
+  protected _onWebSocketMessage(...args: unknown[]): void;
+  protected _onWebSocketError(...args: unknown[]): void;
+  protected _onWebSocketClose(...args: unknown[]): void;
 }
--- src/rpc/websocket_server.js
+++ src/rpc/websocket_server.js
@@ -10,6 +10,46 @@ export class WebSocketRPCServer extends RPCServerBase {
     this._websocket.close();
   }
 
+  _onWebSocketMessage(ev) {
+    let str;
+    if (typeof ev === 'string') {
+      // Note: this is the node 'ws' module
+      str = ev;
+    } else if (typeof ev === 'object' && 'type' in ev) {
+      // Note: this is the node 'websocket' module
+      if (ev.type === 'utf8') {
+        str = ev.utf8Data;
+      } else {
+        throw new Error('Expected TEXT frame.');
+      }
+    } else {
+      throw new Error('Unexpected message.');
+    }
+
+    try {
+      const messages = JSON.parse(str);
+
+      for (let i = 0; i < messages.length; i++) {
+        this._onMessage(messages[i]);
+      }
+    } catch (err) {
+      console.error(
+        'Error while handling RPC message %o from client: %o',
+        ev,
+        err
+      );
+      this.close();
+    }
+  }
+
+  _onWebSocketError(ev) {
+    this.dispose();
+  }
+
+  _onWebSocketClose(ev) {
+    this.dispose();
+  }
+
   constructor(websocket, methods) {
     super(methods);
     this._websocket = websocket;
@@ -21,45 +61,9 @@ export class WebSocketRPCServer extends RPCServerBase {
       this._flush();
     };
 
-    websocket.on('message', (ev) => {
-      let str;
-      if (typeof ev === 'string') {
-        // Note: this is the node 'ws' module
-        str = ev;
-      } else if (typeof ev === 'object' && 'type' in ev) {
-        // Note: this is the node 'websocket' module
-        if (ev.type === 'utf8') {
-          str = ev.utf8Data;
-        } else {
-          throw new Error('Expected TEXT frame.');
-        }
-      } else {
-        throw new Error('Unexpected message.');
-      }
-
-      try {
-        const messages = JSON.parse(str);
-
-        for (let i = 0; i < messages.length; i++) {
-          this._onMessage(messages[i]);
-        }
-      } catch (err) {
-        console.error(
-          'Error while handling RPC message %o from client: %o',
-          ev,
-          err
-        );
-        this.close();
-      }
-    });
-
-    websocket.on('error', (err) => {
-      this.dispose();
-    });
-
-    websocket.on('close', () => {
-      this.dispose();
-    });
+    websocket.on('message', (...args) => this._onWebSocketMessage(...args));
+    websocket.on('error', (...args) => this._onWebSocketError(...args));
+    websocket.on('close', (...args) => this._onWebSocketClose(...args));
   }
 
   _flush() {
