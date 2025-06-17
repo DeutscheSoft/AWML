3bb3aca3fb3f31b6024040e1e7ec2c60173ba5ef
Arne Goedeke <arne@deuso.de>
2025-06-17 10:51:35 +0200

    DynamicValue.wait(): fix type declaration

--- src/dynamic_value.d.ts
+++ src/dynamic_value.d.ts
@@ -8,6 +8,6 @@ export class DynamicValue<T> {
   get isActive(): boolean;
 
   subscribe(subscriber: (value: T) => void, replay?: boolean): () => void;
-  wait(replay?: boolean, predicate: (value: T) => boolean, signal: AbortSignal): Promise<T>;
+  wait(replay?: boolean, predicate?: (value: T) => boolean, signal?: AbortSignal): Promise<T>;
   set(value: T): void | Promise<void>;
 }
